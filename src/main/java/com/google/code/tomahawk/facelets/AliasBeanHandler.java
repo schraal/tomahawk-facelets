c737d0aae8ca2734548a9bf98fcabb4dad7494d7
hilbert.schraal <hilbert.schraal@585b26af-8e36-0410-8478-f9c264fa7052>
2010-03-31 13:49:43 +0000

    issue 11: AliasBean TagHandler duplicates components

--- src/main/java/com/google/code/tomahawk/facelets/AliasBeanHandler.java
+++ src/main/java/com/google/code/tomahawk/facelets/AliasBeanHandler.java
@@ -1,75 +1,65 @@
-/*
- * Copyright 2001-2007 The European Bioinformatics Institute.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.google.code.tomahawk.facelets;
-
-import com.sun.facelets.FaceletContext;
-import com.sun.facelets.FaceletException;
-import com.sun.facelets.tag.TagAttribute;
-import com.sun.facelets.tag.TagHandler;
-import com.sun.facelets.tag.jsf.ComponentConfig;
-import org.apache.myfaces.custom.aliasbean.AliasBean;
-
-import javax.el.ELException;
-import javax.faces.FacesException;
 import javax.faces.application.Application;
 import javax.faces.component.UIComponent;
 import javax.faces.webapp.UIComponentTag;
-import java.io.IOException;
 
-public class AliasBeanHandler extends TagHandler {
+import org.apache.myfaces.custom.aliasbean.AliasBean;
+
+import com.sun.facelets.FaceletContext;
+import com.sun.facelets.tag.MetaRuleset;
+import com.sun.facelets.tag.TagAttribute;
+import com.sun.facelets.tag.jsf.ComponentConfig;
+import com.sun.facelets.tag.jsf.ComponentHandler;
+
+
+/**
+ * Handler for AliasBean Component.
+ * 
+ */
+public class AliasBeanHandler extends ComponentHandler {
 
     private TagAttribute valueAttr;
+
     private TagAttribute aliasAttr;
 
-    public AliasBeanHandler(ComponentConfig tagConfig) {
-        super(tagConfig);
+    public AliasBeanHandler(ComponentConfig aConfig) {
+        super(aConfig);
 
         valueAttr = getRequiredAttribute("value");
         aliasAttr = getRequiredAttribute("alias");
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    protected MetaRuleset createMetaRuleset(Class aClass) {
+        MetaRuleset theRules = super.createMetaRuleset(aClass);
+        theRules = theRules.ignore("value");
+        theRules = theRules.ignore("alias");
+        return theRules;
+    }
 
-    public void apply(FaceletContext ctx, UIComponent parent) throws
-                                                              IOException, FacesException, FaceletException, ELException
-    {
-        Application app = ctx.getFacesContext().getApplication();
+    /**
+     * {@inheritDoc}
+     */
+    protected void onComponentCreated(FaceletContext aContext, UIComponent aComponent, UIComponent aParent) {
+        super.onComponentCreated(aContext, aComponent, aParent);
 
-        AliasBean aliasBean = new AliasBean();
+        AliasBean theComponent = (AliasBean) aComponent;
 
-        String value = valueAttr.getValue();
-        if (UIComponentTag.isValueReference(value))
-        {
-            aliasBean.setValueBinding("value",
-                                      app.createValueBinding(valueAttr.getValue()));
-        } else
-        {
-            aliasBean.setValue(value);
+        Application app = aContext.getFacesContext().getApplication();
+
+        String theValue = valueAttr.getValue();
+        if (UIComponentTag.isValueReference(theValue)) {
+            theComponent.setValueBinding("value", app.createValueBinding(valueAttr.getValue()));
+        } else {
+            theComponent.setValue(theValue);
         }
 
-        String alias = aliasAttr.getValue();
-        if (UIComponentTag.isValueReference(alias)) {
-            aliasBean.setValueBinding("alias",
-                                      app.createValueBinding(aliasAttr.getValue()));
+        String theAlias = aliasAttr.getValue();
+        if (UIComponentTag.isValueReference(theAlias)) {
+            theComponent.setValueBinding("alias", app.createValueBinding(aliasAttr.getValue()));
+        } else {
+            theComponent.setAlias(theAlias);
         }
-        else
-        {
-            aliasBean.setAlias(alias);
-        }
-
-        parent.getChildren().add(aliasBean);
-        this.nextHandler.apply(ctx,aliasBean);
     }
 }
